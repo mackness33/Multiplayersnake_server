d77c7848a5b41a84d21d8768a8e3e8f80b8833d6
Lucy Sarah Dumphy <david.mack@studio.unibo.it>
2022-12-05 19:30:59 +0100

    Change of the port is needed. 3000 is occupied

--- app.js
+++ app.js
@@ -1,12 +1,74 @@
-import express from 'express';
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
-const app = express();
-const server = app.listen(PORT, ()=> {
-    console.log(`listening to: ${PORT}`);
+app.get('/', (req, res) => {
+    res.send("Node Server is running. Yay!!");
+    console.log('in the index');
 })
 
-import { on } from 'socket.io';
+io.on('connection', (socket) => {
+    console.log("Connected successfully ", socket.id);
 
-on('connection', (socket) => {
-    console.log("Connected successfully", socket.id);
+    // socket.on('disconnect', () => {
+    //     console.log('Disconnected successfully ', socket.id);
+    // });
+
+    // socket.on('disconnect', () => {
+    //     console.log('Disconnected successfully ', socket.id);
+    // });
+
+    // socket.on('confirmation', (_, ack) => {
+    //     try {
+    //         games.create(name, player);
+    //         socket.join(name);
+    //         console.log(`${player} created ${name}`);
+    //         ack(true);
+    //     } catch(e) {
+    //         socket.emit('fail', e.message);
+    //         console.error(e);
+    //         ack(false);
+    //     }
+    // });
+
+    // socket.on('create', (name, player, ack) => {
+    //     try {
+    //         games.create(name, player);
+    //         socket.join(name);
+    //         console.log(`${player} created ${name}`);
+    //         ack(true);
+    //     } catch(e) {
+    //         socket.emit('fail', e.message);
+    //         console.error(e);
+    //         ack(false);
+    //     }
+    // });
+
+    // socket.on('join', (name, player) => {
+    //     try {
+    //         games.join(name, player);
+    //         socket.join(name);
+    //         console.log(`${player} joined ${room}`);
+    //         ack(true);
+    //     } catch (e) {
+    //         socket.emit('fail', e.message);
+    //         console.error(e);
+    //         ack(false);
+    //     }
+    // });
+
+    // socket.on('leave', (name, player) => {
+    //     try {
+    //         games.leave(name, player);
+    //         socket.leave(name);
+    //         console.log(`${player} leaved ${room}`);
+    //     } catch (e) {
+    //         socket.emit('fail', e.message);
+    //         console.error(e);
+    //     }
+    // });
 })
+
+server.listen(3002,  ()=> {
+    console.log(`listening to: ${3002}`);
+});
